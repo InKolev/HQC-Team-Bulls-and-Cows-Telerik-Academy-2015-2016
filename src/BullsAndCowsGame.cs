8ee1467906972b61c63bc0439f37fbfa3f93ec5b
vot24100 <mladen.m.savov@gmail.com>
2015-09-07 11:33:12 +0300

    Renaming Program.cs to BullsAndCowsGame.cs. Initial rearrangement of the code.

--- src/BullsAndCowsGame.cs
+++ src/BullsAndCowsGame.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace BullsCows
+namespace BullsAndCows
 {
-//tova mie purvata Csharp programa v jivota
-// obache dobre stana .i ba4ka na 100%
-    class Program
+    class BullsAndCowsGame
     {
-        
-
-
+        static int number, attempts;
+        static bool notCheated;
+        static SortedList<int, string> klasirane = new SortedList<int, string>();
+        static Random rr = new Random();
+        static string ch;
 
         static void WriteAbout()
         {
@@ -19,31 +19,35 @@ namespace BullsCows
             Console.WriteLine("Use 'top' to view the top scoreboard, 'restart' to start a new game and 'help' to cheat and 'exit' to quit the game.");
         }
 
-        static int number, attempts;
-        static bool notCheated;
-
         private static void tryAddToScoreboard()
         {
-            if (klasirane.Count < 5 || klasirane.ElementAt(4).Key > attempts) {
+            if (klasirane.Count < 5 || klasirane.ElementAt(4).Key > attempts) 
+            {
                 Console.WriteLine("Please enter your name for the top scoreboard: ");
                 string name = Console.ReadLine().Trim();
-
                 klasirane.Add(attempts, name);
+
                 if (klasirane.Count == 6)
+                {
                     klasirane.RemoveAt(5);
+                }                    
 
                 DisplayTop();
             }
         }
-        static void ProcessWin() {
+
+        static void ProcessWin() 
+        {
             Console.WriteLine("Congratulations! You guessed the secret number in {0} attempts.", attempts);
             
-            if (notCheated) {
+            if (notCheated) 
+            {
                 tryAddToScoreboard();
             }
 
             StartNewGame();
         }
+
         static void ProcessGuess(int guess)
         {
             if (guess == number)
@@ -61,10 +65,7 @@ namespace BullsCows
                 int[] digs = new int[10];
                 for (int d = 0; d < 10; d++)
                     digs[d] = 0;
-
-
-
-
+                
                 for (int i = 0; i < 4; i++)
                     if (!isBull[i])
                         digs[snum[i] - '0']++;
@@ -78,10 +79,7 @@ namespace BullsCows
                             cows++;
                             digs[sguess[i] - '0']--;
                         }
-                    }
-
-
-
+                    }                    
                 }
 
                 Console.WriteLine("Wrong number! Bulls: {0}, Cows: {1}", bulls, cows);
@@ -89,19 +87,16 @@ namespace BullsCows
             }
         }
 
-        static SortedList<int, string> klasirane = new SortedList<int,string>();
         static void DisplayTop()
         {
             if (klasirane.Count() > 0)
             {
                 Console.WriteLine("Scoreboard:");
                 int i = 1;
+                
                 foreach (var t in klasirane)
                 {
                     Console.WriteLine("{0}. {1} --> {2} guesses", i, t.Value, t.Key);
-
-
-
                 }
             }
             else
@@ -109,17 +104,18 @@ namespace BullsCows
                 Console.WriteLine("Top scoreboard is empty.");
             }
         }
-
-        static Random rr = new Random();        
-        static void StartNewGame() {
+   
+        static void StartNewGame() 
+        {
             WriteAbout();
             number = rr.Next(1000, 10000);
             attempts = 1;
             notCheated = true;
             ch = "XXXX";
         }
-        static string ch;
-        static void Cheat() {
+     
+        static void Cheat() 
+        {
             notCheated = false;
             if (ch.Contains('X')) {
                 int i;
@@ -137,10 +133,6 @@ namespace BullsCows
         static bool ReadAction()
         {
             Console.WriteLine("Enter your guess or command: ");
-
-
-
-
             string line = Console.ReadLine().Trim();
             Regex patt = new Regex("[1-9][0-9][0-9][0-9]");
                     
@@ -155,9 +147,6 @@ namespace BullsCows
                     break;
                 
                 case "help":
-
-
-
                     Cheat();
                     break;
                 
@@ -181,7 +170,7 @@ namespace BullsCows
             return true;
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
             StartNewGame();
             while (ReadAction()) ;
