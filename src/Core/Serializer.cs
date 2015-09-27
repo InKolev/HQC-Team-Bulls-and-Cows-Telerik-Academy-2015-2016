fc3a2004fcaf238416c653521df7b10f0f76f8ba
mdraganov <mariyandraganov@gmail.com>
2015-09-27 17:56:42 +0300

    added StreamWriter

--- src/Core/Serializer.cs
+++ src/Core/Serializer.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BullsAndCows.Core
+﻿namespace BullsAndCows.Core
 {
-    internal class Serializer
-    {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
+    using BullsAndCows.Interfaces;
+    using System.IO;
+
+    internal class Serializer : ISerializer
+    {
+        private const string FilePath = @"../../scores.txt";
+
+        public SortedList<int, string> Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(SortedList<int, string> scores)
+        {
+            var writer = new StreamWriter(FilePath);
+
+            using (writer)
+            {
+
+            }
+        }
     }
 }
