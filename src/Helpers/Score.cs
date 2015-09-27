348eee222b0a4c94ff27b814afeeef7161391974
mdraganov <mariyandraganov@gmail.com>
2015-09-27 18:05:13 +0300

    Added Score class.

---
+++ src/Helpers/Score.cs
@@ -0,0 +1,19 @@
+﻿namespace BullsAndCows.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    internal class Score: IComparable<Score>
+    {
+        public int NumberOfGuesses { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int CompareTo(Score otherScore)
+        {
+            return this.NumberOfGuesses.CompareTo(otherScore.NumberOfGuesses);
+        }
+    }
+}
