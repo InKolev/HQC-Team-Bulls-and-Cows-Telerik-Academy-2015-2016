f562eddbf98f858d51354d10bc1c2befe6d744d9
mdraganov <mariyandraganov@gmail.com>
2015-09-29 10:45:30 +0300

    styleCop improvements

--- src/Helpers/ConsoleNotifier.cs
+++ src/Helpers/ConsoleNotifier.cs
@@ -1,14 +1,59 @@
-﻿using BullsAndCows.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BullsAndCows.Helpers
+﻿namespace BullsAndCows.Helpers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Interfaces;
+
     internal class ConsoleNotifier : IScoreNotifier, INotifier
     {
         private int WindowWidth { get; set; } = Console.WindowWidth;
+        
+        public void DisplayScores(List<Score> scores)
+        {
+            int padLeftWidth = scores.Max(x => x.PlayerName.Length);
+            int scoreBoardWidth = padLeftWidth + 17;
+
+            Console.WriteLine('┌' + new String('─', scoreBoardWidth) + '┐');
+            Console.Write("├─" + new String('─', (int)Math.Ceiling((double)(padLeftWidth - 3) / 2)) + "Player" + new String('─', (padLeftWidth - 3) / 2));
+            Console.WriteLine(new String('─', 4) + "Attempts" + "─┤");
+            Console.WriteLine('│' + new String(' ', scoreBoardWidth) + '│');
+
+            int position = 1;
+
+            foreach (var score in scores)
+            {
+                Console.WriteLine(String.Format(
+                                        "│{0}. {1} ── {2} guesses│",
+                                        position++,
+                                        score.PlayerName.PadLeft(padLeftWidth),
+                                        score.NumberOfGuesses.ToString().PadRight(2)));
+            }
+
+            Console.WriteLine('└' + new String('─', scoreBoardWidth) + '┘');
+        }
+
+        public void Notify(string message)
+        {
+            switch (message)
+            {
+                case "IntroductionCall":
+                    {
+                        this.DisplayIntroductionMessage();
+                        break;
+                    }
+                case "CommandsCall":
+                    {
+                        this.DisplayCommandsList();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine(message);
+                        break;
+                    }
+            }
+        }
 
         private void DisplayIntroductionMessage()
         {
@@ -27,50 +72,6 @@ namespace BullsAndCows.Helpers
             Console.WriteLine("\"exit\"     - to quit the game and close the application.");
             Console.WriteLine("\"help\"     - to unveil a random secret digit (if you want to be a cheater).");
             Console.WriteLine("\"top\"      - to view the top scoreboard.");
-        }
-
-        public void DisplayScores(List<Score> scores)
-        {
-            int position = 1;
-            int padLeftWidth = scores.Max(x => x.PlayerName.Length);
-            int scoreBoardWidth = padLeftWidth + 17;
-
-            Console.WriteLine('┌' + new String('─', scoreBoardWidth) + '┐');
-            Console.Write("├─" + new String('─', (int)Math.Ceiling((double)(padLeftWidth - 3) / 2)) + "Player" + new String('─', (padLeftWidth - 3) / 2));
-            Console.WriteLine(new String('─', 4) + "Attempts" + "─┤");
-            Console.WriteLine('│' + new String(' ', scoreBoardWidth) + '│');
-
-            foreach (var score in scores)
-            {
-                this.Notify(String.Format("│{0}. {1} ── {2} guesses│",
-                    position++,
-                    score.PlayerName.PadLeft(padLeftWidth),
-                    score.NumberOfGuesses.ToString().PadRight(2)));
-            }
-
-            Console.WriteLine('└' + new String('─', scoreBoardWidth) + '┘');
-        }
-
-        public void Notify(string message)
-        {
-            switch (message)
-            {
-                case "IntroductionCall":
-                    {
-                        DisplayIntroductionMessage();
-                        break;
-                    }
-                case "CommandsCall":
-                    {
-                        DisplayCommandsList();
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine(message);
-                        break;
-                    }
-            }
         }
     }
 }
