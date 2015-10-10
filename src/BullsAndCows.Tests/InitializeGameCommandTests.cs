c1eaa645ede2999b349e003875e27a947b2757df
vot24100 <mladen.m.savov@gmail.com>
2015-10-10 11:36:03 +0300

    Added Unit Test project BullsAndCows.Tests. Added InitializeGameCommandTests class. Added first unit test InitializeGameCommandShouldSetGuessAttemptsMaxValue

---
+++ src/BullsAndCows.Tests/InitializeGameCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Helpers;
+using BullsAndCows.Helpers.Commands;
+using BullsAndCows.Helpers.Misc;
+using BullsAndCows.Interfaces;
+using BullsAndCows.Core;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class InitializeGameCommandTests
+    {
+        [TestMethod]
+        public void InitializeGameCommandShouldSetGuessAttemptsMaxValue()
+        {
+            IDataState data = new Data();
+            INotifier notifier = new ConsoleNotifier();
+            INumberGenerator numberGenerator = new RandomNumberGenerator();
+            InitializeGameCommand initializeCommand = new InitializeGameCommand(data, notifier, numberGenerator);
+            initializeCommand.Execute();
+            Assert.AreEqual(25, data.GuessAttemptsMaxValue);
+        }
+    }
+}
