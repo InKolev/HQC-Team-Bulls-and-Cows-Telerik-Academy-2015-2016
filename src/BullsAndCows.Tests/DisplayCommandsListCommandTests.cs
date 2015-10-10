e9a7bd3ab07a35e18463f7c0eed5bc1f7e5a06fc
vot24100 <mladen.m.savov@gmail.com>
2015-10-10 17:16:39 +0300

    Added DisplayCommandsListCommandTests.cs

---
+++ src/BullsAndCows.Tests/DisplayCommandsListCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Helpers;
+using BullsAndCows.Helpers.Commands;
+using BullsAndCows.Helpers.Misc;
+using BullsAndCows.Interfaces;
+using BullsAndCows.Core;
+using Moq;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class DisplayCommandsListCommandTests
+    {
+        [TestMethod]
+        public void DisplayCommandsListCommandShouldCallNotifier()
+        {
+            var mockedNotifier = new Mock<INotifier>();
+            IDataState data = new Data();
+            ICommand displayCommandsList = new DisplayCommandsListCommand(mockedNotifier.Object);
+            displayCommandsList.Execute();
+            mockedNotifier.Verify(m => m.Notify(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
