a6d7b756a122ded40ba34628ff9051838ceaa2e5
vot24100 <mladen.m.savov@gmail.com>
2015-10-10 16:48:00 +0300

    Added DisplayScoreboardCommandTests.cs

---
+++ src/BullsAndCows.Tests/DisplayScoreboardCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Helpers;
+using BullsAndCows.Helpers.Commands;
+using BullsAndCows.Helpers.Misc;
+using BullsAndCows.Interfaces;
+using BullsAndCows.Core;
+using Moq;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class DisplayScoreboardCommandTests
+    {
+        [TestMethod]
+        public void DisplayScoreboardCommandShouldCallScoreboard()
+        {
+            var mockedScoreboard = new Mock<IScoreboard>();
+            ICommand displayScoreboard = new DisplayScoreboardCommand(mockedScoreboard.Object);
+            displayScoreboard.Execute();
+            mockedScoreboard.Verify(m => m.DisplayTopScores(), Times.Once);
+        }
+    }
+}
