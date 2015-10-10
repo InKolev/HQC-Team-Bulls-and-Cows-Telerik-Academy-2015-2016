0fbee12711e8fbe468a8e7e4c0618384ed69ad01
vot24100 <mladen.m.savov@gmail.com>
2015-10-10 16:42:48 +0300

    Added DisplayMessageCommandTests.cs

---
+++ src/BullsAndCows.Tests/DisplayMessageCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Helpers;
+using BullsAndCows.Helpers.Commands;
+using BullsAndCows.Helpers.Misc;
+using BullsAndCows.Interfaces;
+using BullsAndCows.Core;
+using Moq;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class DisplayMessageCommandTests
+    {        
+        [TestMethod]
+        public void DisplayMessageCommandShouldCallNotifier()
+        {
+            var mockedNotifier = new Mock<INotifier>();
+            IDataState data = new Data();
+            ICommand displayMessage = new DisplayMessageCommand(mockedNotifier.Object, true, "");
+            displayMessage.Execute();
+            mockedNotifier.Verify(m => m.Notify(""), Times.Once);
+        }
+    }
+}
