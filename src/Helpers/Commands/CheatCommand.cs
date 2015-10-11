a159f7507dadd706f408e3dfc7265fdf758b3dc4
InKolev <in.kolev.b@gmail.com>
2015-10-11 23:44:38 +0300

    Added continuous integration with Appveyor.

--- src/Helpers/Commands/CheatCommand.cs
+++ src/Helpers/Commands/CheatCommand.cs
@@ -10,8 +10,17 @@ namespace BullsAndCows.Helpers.Commands
     using BullsAndCows.Interfaces;
     using BullsAndCows.Core;
 
+    /// <summary>
+    /// The concrete implementation for CheatCommand.
+    /// </summary>
     internal class CheatCommand : ICommand
     {
+        /// <summary>
+        /// The constructor for cheat command.
+        /// </summary>
+        /// <param name="data">The data object which holds the core game data.</param>
+        /// <param name="notifier">The object that notifies the user.</param>
+        /// <param name="numberGenerator">A random number generator</param>
         public CheatCommand(IDataState data, INotifier notifier, INumberGenerator numberGenerator)
         {
             this.Data = data;
@@ -19,12 +28,25 @@ namespace BullsAndCows.Helpers.Commands
             this.NumberGenerator = numberGenerator;
         }
 
+        /// <summary>
+        /// Data holder object
+        /// </summary>
         private IDataState Data { get; set; }
 
+        /// <summary>
+        /// The object that suits for user notifications.
+        /// </summary>
         private INotifier Notifier { get; set; }
 
+        /// <summary>
+        /// Random number generator for deciding which digit to be exposed from the answer.
+        /// </summary>
         private INumberGenerator NumberGenerator { get; set; }
 
+        /// <summary>
+        /// The core logic for cheat command execution
+        /// </summary>
+        /// <returns></returns>
         public bool Execute()
         {
             this.Data.HasCheated = true;
