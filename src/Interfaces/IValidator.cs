30d7af9b6b3124d9ebccec621f2618828972521c
vot24100 <mladen.m.savov@gmail.com>
2015-10-11 21:49:44 +0300

    Finished code documentation for Interfaces

--- src/Interfaces/IValidator.cs
+++ src/Interfaces/IValidator.cs
@@ -1,7 +1,15 @@
 ﻿namespace BullsAndCows.Interfaces
 {
+    /// <summary>
+    /// Interface for validator
+    /// </summary>
     public interface IValidator
     {
+        /// <summary>
+        /// Validates a name
+        /// </summary>
+        /// <param name="name">The name to be validated</param>
+        /// <returns>If the name was validated</returns>
         bool ValidateName(string name);
     }
 }
