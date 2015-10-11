cd7f51d655888b59c8cf3353cae8ffa1b7357b3b
mdraganov <mariyandraganov@gmail.com>
2015-10-12 01:20:17 +0300

    Updated ScoreTests/ScoreboardTests.cs

--- src/BullsAndCows.Tests/ScoreTests/ScoreboardTests.cs
+++ src/BullsAndCows.Tests/ScoreTests/ScoreboardTests.cs
@@ -16,35 +16,38 @@ namespace BullsAndCows.Tests
         private IScoreSerializer serializer;
         private IActionsReader actionsReader;
         private IList<Score> scores;
-        
-        [TestInitialize]
-        public void CreteMocks()
-        {
-            scores = new List<Score>();
-            scores.Add(new Score(1, "Score1", 5));
-            scores.Add(new Score(2, "Score2", 6));
-            scores.Add(new Score(3, "Score3", 7));
 
+        public ScoreboardTests()
+        {
             var mockNotifier = new Mock<IScoreNotifier>();
             mockNotifier.Setup(n => n.Notify(It.IsAny<string>())).Verifiable();
-            notifier = mockNotifier.Object;
+            this.notifier = mockNotifier.Object;
 
             var mockReader = new Mock<IActionsReader>();
             mockReader.Setup(r => r.Read()).Returns("Score4");
-            actionsReader = mockReader.Object;
+            this.actionsReader = mockReader.Object;
+        }
+
+        [TestInitialize]
+        public void CreteScoresList()
+        {
+            this.scores = new List<Score>();
+            this.scores.Add(new Score(1, "Score1", 5));
+            this.scores.Add(new Score(2, "Score2", 6));
+            this.scores.Add(new Score(3, "Score3", 7));
         }
 
         [TestMethod]
         public void ScoreboardShouldAddNewScores()
         {
             var mockSerializer = new Mock<IScoreSerializer>();
-            mockSerializer.Setup(s => s.Load()).Returns(scores);
+            mockSerializer.Setup(s => s.Load()).Returns(this.scores);
             mockSerializer.Setup(s => s.Save(It.Is<List<Score>>(l => l.Count == 4))).Verifiable();
             serializer = mockSerializer.Object;
 
             var scorBoard = new Scoreboard(notifier, serializer, actionsReader);
-
             scorBoard.AddToScoreboard(4, 5);
+
             mockSerializer.Verify();
         }
     }
