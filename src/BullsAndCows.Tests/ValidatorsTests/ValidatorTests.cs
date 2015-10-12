bbc0cae34a4cbe6ed1434b8996d43211b6877824
mdraganov <mariyandraganov@gmail.com>
2015-10-12 07:50:59 +0300

    Added ValidatorTests.cs

---
+++ src/BullsAndCows.Tests/ValidatorsTests/ValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Helpers.Validators;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class ValidatorTests
+    {
+        [TestMethod]
+        public void GetValidatorShouldReturnOnlyOneInstance()
+        {
+            var validator = Validator.GetValidator();
+            var secondValidator = Validator.GetValidator();
+
+            Assert.AreSame(validator, secondValidator);
+        }
+    }
+}
